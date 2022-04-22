04709f2d5b5ad920df351b35ad5d9fc9cad544f9
Sebastian Szeszko <sebastian.szeszko@primigenius.pl>
2022-04-22 17:19:17 +0200

    add craete Tree method

--- createTree.js
+++ createTree.js
@@ -1,27 +1,31 @@
 function TreeNode(val, left, right) {
-  this.val = val === undefined ? 0 : val;
-  this.left = left === undefined ? null : left;
-  this.right = right === undefined ? null : right;
+  this.val = val === undefined ? 0 : val
+  this.left = left === undefined ? null : left
+  this.right = right === undefined ? null : right
+}
+
+const insertValue = (root, q, val) => {
+  const node = new TreeNode(val)
+  if (root == null) {
+    root = node
+  } else if (q[0].left == null) {
+    q[0].left = node
+  } else {
+    q[0].right = node
+    q.shift()
+  }
+
+  q.push(node)
+  return [root, q]
 }
 
 const buildTreeFromArray = (arr) => {
-  if (!arr.length) return new TreeNode();
-  const root = new TreeNode(arr.shift());
-  let it = 1;
-  for (let i = 1; i < arr.length; i++) {
-    let temp = new TreeNode(arr[i]);
-    if (i + i <= arr.length) temp.left = new TreeNode(arr[i + i - 1]);
-    if (i + i < arr.length) temp.right = new TreeNode(arr[i + i]);
+  let root = null
+  let q = []
+  for (val of arr) {
+    ;[root, q] = insertValue(root, q, val)
   }
-  return root;
-};
+  return root
+}
 
-console.log(buildTreeFromArray([]));
-
-module.exports.buildTreeFromArray;
-
-// 0               1
-// 1          2          3
-// 2        4    5     6     7
-// 4      8  9 10 11 12 13  14  15
-//     16 17
+module.exports.buildTreeFromArray
